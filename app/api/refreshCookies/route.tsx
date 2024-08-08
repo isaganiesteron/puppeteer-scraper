1d9fe0cfff547cb8de2b08d3d64294de1df3bf42
Isagani Esteron <isaganiesteron@ymail.com>
2024-08-09 07:29:07 +0800

    added logging

--- app/api/refreshCookies/route.tsx
+++ app/api/refreshCookies/route.tsx
@@ -1,26 +1,26 @@
-export const maxDuration = 60;
-import { NextResponse } from 'next/server';
-import fetchExpediaCookies from '@/app/utils/fetchExpediaCookies';
-import { sql } from '@vercel/postgres';
+export const maxDuration = 60
+import { NextResponse } from "next/server"
+import fetchExpediaCookies from "@/app/utils/fetchExpediaCookies"
+import { sql } from "@vercel/postgres"
 
 export async function POST(request: Request) {
-  console.log('***Refresh Expedia Cookies***');
-  try {
-    let authorization;
-    const headers = request.headers;
-    headers.forEach((value, name) => {
-      if (name === 'authorization') authorization = value;
-    });
-    if (authorization !== process.env.API_ENPOINT_KEY)
-      return NextResponse.json('Unauthorized', { status: 401 });
-    const expediaCookie = await fetchExpediaCookies();
-    await sql.query('DELETE FROM cookies');
-    const sqlString = `INSERT INTO cookies (value) VALUES ('${expediaCookie}')`;
-    const result = await sql.query(sqlString);
-    if (result.rowCount === 1)
-      return NextResponse.json({ success: true, message: 'Cookies refreshed' });
-    return NextResponse.json({ success: false, message: 'Failed to refresh cookies' });
-  } catch (error: any) {
-    return NextResponse.json({ success: false, message: error.message || 'error' });
-  }
+	console.log("***Refresh Expedia Cookies***")
+	try {
+		let authorization
+		const headers = request.headers
+		headers.forEach((value, name) => {
+			if (name === "authorization") authorization = value
+		})
+		console.log("authorization", authorization)
+		console.log("process.env.API_ENPOINT_KEY", process.env.API_ENPOINT_KEY)
+		if (authorization !== process.env.API_ENPOINT_KEY) return NextResponse.json("Unauthorized", { status: 401 })
+		const expediaCookie = await fetchExpediaCookies()
+		await sql.query("DELETE FROM cookies")
+		const sqlString = `INSERT INTO cookies (value) VALUES ('${expediaCookie}')`
+		const result = await sql.query(sqlString)
+		if (result.rowCount === 1) return NextResponse.json({ success: true, message: "Cookies refreshed" })
+		return NextResponse.json({ success: false, message: "Failed to refresh cookies" })
+	} catch (error: any) {
+		return NextResponse.json({ success: false, message: error.message || "error" })
+	}
 }
